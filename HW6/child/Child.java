32dfe15c474087f1524507cff47268c943d5fc2e
Trevaughn McGregor <trevaughnmcgregor@yahoo.com>
2017-10-16 00:05:03 -0400

    HW6~UPDATE
    correction to hw6,now includes packages

--- HW6/child/Child.java
+++ HW6/child/Child.java
@@ -1,5 +1,5 @@
-
-import java.util.*;
+package child;
+import parent.Parent;
 
 public class Child extends Parent
 {
--- HW6/parent/Parent.java
+++ HW6/parent/Parent.java
@@ -1,3 +1,5 @@
+package parent;
+
 public class Parent
 {
   protected String str = "hello";
